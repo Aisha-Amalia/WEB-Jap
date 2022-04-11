0556d3e3f3115333994c217a61ff7cc718254df5
Aisha-Amalia <79995663+Aisha-Amalia@users.noreply.github.com>
2022-04-11 10:23:11 +0300

    WEB-Jap

---
+++ WEB/script.js
@@ -0,0 +1,8 @@
+
+
+function myFunc() {
+
+document.getElementById('ok').style.transform = "translateY(-900px)";
+setTimeout(()=> document.getElementById('ok').style.transform = "translateY(0px)", 2000);
+}
+
